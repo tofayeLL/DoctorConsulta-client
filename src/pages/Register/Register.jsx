3edc8d5a37969a3ce7d070d2af139ab7b86d3433
tofayeLL <ahmedtofayel132@gmail.com>
2024-05-10 17:49:36 +0600

    added register section register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,96 @@
+import { useState } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
+    const [showPass, setShowPass] = useState(false);
+    const [errorRegister, setErrorRegister] = useState('');
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, photo, password);
+    }
+
+
+
+
     return (
         <div>
-            <h1>This is from register page</h1>
-            
+
+            <div className="flex flex-col  justify-center items-center bg-cyan-400 ">
+
+
+                <div className="flex flex-col justify-center  w-full max-w-lg mx-auto space-y-4 my-6 bg-white shadow-xl lg:px-0   px-5  lg:py-12 py-6 rounded-md " >
+
+
+
+                    <div className=" mx-auto text-cyan-700">
+                        <h1 className="lg:text-4xl text-xl font-bold mb-6">Create an account</h1>
+                    </div>
+
+
+                    <form onSubmit={handleRegister} className="lg:w-[70%] w-full mx-auto space-y-5 rounded-lg " >
+
+                        <div >
+                            <p className="mb-1 font-medium">User Name</p>
+                            <input type="text" name="name" placeholder="Username" className="input input-bordered w-full" />
+                        </div>
+
+                        <div >
+                            <p className="mb-1 font-medium">Photo Url</p>
+                            <input type="text" name="photo" placeholder="photo url" className="input input-bordered w-full" />
+                        </div>
+
+                        <div>
+                            <p className="mb-1 font-medium">User Email</p>
+                            <input type="email" name="email" placeholder="Email address" className="input input-bordered w-full" required />
+                        </div>
+
+                        <div >
+                            <p className="mb-1 font-medium"> Password</p>
+
+                            <div className="relative">
+                                <input type={showPass ? 'text' : 'password'} name="password" placeholder="password" className="input input-bordered w-full" required />
+
+                                <span onClick={() => setShowPass(!showPass)} className="absolute top-3 right-3">{showPass ? <FiEyeOff className="text-xl" ></FiEyeOff> : <FiEye className="text-xl"></FiEye>}</span>
+
+                            </div>
+                        </div>
+
+                        <div className="text-center">
+
+                            <p className="text-red-500 lg:text-lg text-sm font-semibold">{errorRegister.replace('auth/', '')}</p>
+                        </div>
+
+
+                        <div>
+                            <button className="btn text-white text-lg w-full bg-cyan-700" type="submit">Register</button>
+                        </div>
+
+
+
+
+                    </form>
+
+
+
+
+                    <div className="text-center ">
+                        <p className="font-medium mt-6  lg:text-base text-sm mr-2">have an account ?   Please  <Link to={'/login'} className="btn-active text-cyan-800 btn-link">Login</Link></p>
+                    </div>
+
+
+
+
+                </div>
+            </div>
+
         </div>
     );
 };
